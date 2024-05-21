70d4d45f8fa507d7933f1d6cf371906430bb85b7
Gaurav151008 <gauravpatil200080@gmail.com>
2024-05-21 22:55:10 +0530

    frontend up1

---
+++ frontend/build/precache-manifest.7b056ca89d300f568dd85d7273ea4c8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ac3bd6f4294aee63b8b5e490f8ced3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97d2b8033be5e823ce5f",
+    "url": "/static/css/main.c585ed2f.chunk.css"
+  },
+  {
+    "revision": "9275a66931de0bce65d2",
+    "url": "/static/js/2.cd6724c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cd6724c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d2b8033be5e823ce5f",
+    "url": "/static/js/main.c5f66581.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
