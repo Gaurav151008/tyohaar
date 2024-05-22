f7df17c8523ef8d18f0a549d04df45c572706a99
Gaurav151008 <gauravpatil200080@gmail.com>
2024-05-22 16:08:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 // Import Router
 const authRouter = require("./routes/auth");
@@ -46,7 +45,6 @@ mongoose
 // Middleware
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
